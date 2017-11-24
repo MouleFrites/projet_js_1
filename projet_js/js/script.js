11f633b04e544489c051dace484c3ca56ef044da
MouleFrites <33952878+MouleFrites@users.noreply.github.com>
2017-11-24 22:14:12 +0100

    change1

--- projet_js/js/script.js
+++ projet_js/js/script.js
@@ -1,15 +1,22 @@
+var presence;
+presence = 0;
+
 window.setInterval(function() {
-var test = document.getElementById('checkbox').value;
-console.log (test);
+console.log (presence);
 }, 1000);
 
-var present = document.forms['checkboxform'].elements[i].value;
-var page1 = document.getElementById('contactinf').style.visibility='visible';
+function test(){
+  presence = 1;
+  };
+function test2(){
+    if (presence == 1)
+      alert("on taime")
+    if (presence == 0)
+      alert("ntm pd")
+    };
+/*var page1 = document.getElementById('contactinf').style.visibility='visible';
 var page2 = document.getElementById('notthere').style.visibility='hidden';
 var page3 = document.getElementById('there').style.visibility='hidden';
 var page4 = document.getElementById('nothanx').style.visibility='hidden';
 var page5 = document.getElementById('playlist').style.visibility='hidden';
-var page6 = document.getElementById('thanx').style.visibility='hidden';
-
-if var present
-je suis dans le js
+var page6 = document.getElementById('thanx').style.visibility='hidden'; */
