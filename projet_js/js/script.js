8879878ec8d9d97bb58489d8ba02da1e494fde00
MouleFrites <33952878+MouleFrites@users.noreply.github.com>
2017-11-25 16:10:26 +0100

    change2

--- projet_js/js/script.js
+++ projet_js/js/script.js
@@ -1,15 +1,5 @@
 var presence = 0;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-window.setInterval(function() {
-console.log (presence);
-}, 1000); //wtf c'est quoi ce 1000
-
->>>>>>> 20c42752b7dae218607da70dc3ebc5aaf65a707b
-=======
->>>>>>> bcdb879ea324cfc0e4cf35caa614e331d83594f6
 function test(){
   presence = 1;
   };
@@ -19,16 +9,12 @@ function test2(){
     if (presence == 0)
       alert("ntm pd")
     };
-<<<<<<< HEAD
-var page1 = document.getElementById('contactinf').style.visibility='visible';
-=======
 
 
 
 
 
 /*var page1 = document.getElementById('contactinf').style.visibility='visible';
->>>>>>> 20c42752b7dae218607da70dc3ebc5aaf65a707b
 var page2 = document.getElementById('notthere').style.visibility='hidden';
 var page3 = document.getElementById('there').style.visibility='hidden';
 var page4 = document.getElementById('nothanx').style.visibility='hidden';
