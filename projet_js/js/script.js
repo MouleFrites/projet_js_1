a92583eb0b52082def5a240bb416bed6509e6d2b
MouleFrites <33952878+MouleFrites@users.noreply.github.com>
2017-11-27 10:58:33 +0100

    changejsp1

--- projet_js/js/script.js
+++ projet_js/js/script.js
@@ -1,18 +1,29 @@
-setTimeout(function hide() {
+
   var presence = 0;
-  var page2 = document.getElementById('notthere').style.display ='none';
-  var page4 = document.getElementById('nothanx').style.display ='none';
-}, 1000);
+  var page = 0;
+
+function testfinal(){
+  if (page == 0) {
+    var page1 = document.getElementById('contactinf').style.display='block';
+    var page2 = document.getElementById('notthere').style.display ='none';
+    var page3 = document.getElementById('there').style.display='none';
+    var page4 = document.getElementById('nothanx').style.display ='none';
+    var page5 = document.getElementById('playlist').style.display='none';
+    var page6 = document.getElementById('thanx').style.display='none';
+  } else if ( page == 1 ){
+    page2 = 'block'
+  }
+}
 
 
-function test(){
+function button1(){
   presence = 1;
   };
-function test2(){
-    if (presence == 1)
-      alert("on taime")
-    if (presence == 0)
-      alert("ntm pd")
+function button2(){
+    if (presence == 1) {
+      page = 1;
+      testfinal();
+    };
     };
 
 function test3(){
@@ -21,9 +32,12 @@ function test3(){
 };
 
 function test4(){
-
-  var page4 ='block';
+    page4 = 'block';
 };
+
+/*window.setInterval(function() {
+console.log (presence);
+}, 1000); */
 
 /* var page1 = document.getElementById('contactinf').style.visibility='visible';
 var page2 = document.getElementById('notthere').style.visibility='hidden';
